5fe614d3899bdda86cd40c4f7be0ce33d58502e1
CAONIMEIDEXINGFU <839714319@QQ.COM>
2017-07-07 20:48:50 +0800

    manage

---
+++ qs-manage/src/main/java/com/qs/qs_manage/App.java
@@ -0,0 +1,13 @@
+package com.qs.qs_manage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

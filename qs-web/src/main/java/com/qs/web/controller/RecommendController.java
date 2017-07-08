c758e3dc3cbe0efbe364f18709df800284daf24c
wuyue <wy732858140@163.com>
2017-07-08 17:41:04 +0800

    reconmmend改变

--- qs-web/src/main/java/com/qs/web/controller/RecommendController.java
+++ qs-web/src/main/java/com/qs/web/controller/RecommendController.java
@@ -1,8 +1,24 @@
 package com.qs.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.qs.web.pojo.User;
+import com.qs.web.service.RecommendService;
 
 @Controller
 public class RecommendController {
 
+	@Autowired
+	private RecommendService recommendService;
+	@RequestMapping("/recommend")
+	public String recommend(Model model){
+		List<User> users = recommendService.findSelectedMember();
+		model.addAttribute("users",users);
+		return "index";
+	}
 }

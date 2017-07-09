41c02ca1060cb10b92fdae3a58d0b3a7931bd882
wuyue <wy732858140@163.com>
2017-07-09 19:58:30 +0800

    some change

--- qs-manage/qs-manage-mapper/src/main/java/com/qs/manage/mapper/UserMapper.java
+++ qs-manage/qs-manage-mapper/src/main/java/com/qs/manage/mapper/UserMapper.java
@@ -3,14 +3,10 @@ package com.qs.manage.mapper;
 import java.util.List;
 
 import com.qs.common.mapper.SysMapper;
-<<<<<<< HEAD
 
 import qs.manage.pojo.SelectedMember;
 import qs.manage.pojo.SelectedMemberInfo;
-import qs.manage.pojo.User;
-=======
 import com.qs.manage.pojo.User;
->>>>>>> 26575bd8b0db3d0c39c94a0c81c22c85060bbd1e
 
 public interface UserMapper extends SysMapper<User> {
 
